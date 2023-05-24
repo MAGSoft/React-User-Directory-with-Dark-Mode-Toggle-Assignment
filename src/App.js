9b006096be7c380a686c94f1fff013ccc5ec9a7e
khansa20 <khansamueen@gmail.com>
2023-05-24 19:24:06 +0500

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 const App = () => {
-  return <div className="container"></div>;
+  return <div className="App">{/* Your app content goes here */}</div>;
 };
 
 export default App;
